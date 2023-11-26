7c9bbf58c6bc7f053206314a431d06a4e2b71121
Exx1piee <130587927+Exx1piee@users.noreply.github.com>
2023-11-26 19:33:24 +0200

    bb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,16 @@ import { Statistics } from './statistics/statistics.jsx';
 import {FriendList} from './friendList/friendlist.jsx';
 import user from '../user.json';
 import data from '../data.json';
-import friends from '../friends.json'
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+import {TransactionHistory} from './transactionHistory/transactionhistory.jsx';
 export const App = () => {
   return (
     <div className="profile">
       <Profile user={user} />
        <Statistics title="Upload stats" stats={data} />
-       <FriendList title="Friends" friends={friends} /> 
+       <FriendList title="Friends" friends={friends} />
+       <TransactionHistory title="Transation History" items={transactions}/>
     </div>
   );
 
